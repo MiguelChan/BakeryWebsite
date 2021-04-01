fa5875efb83d907b34ee7402501fbe419dc4e623
Miguel Chan <miguel.chanc1991@gmail.com>
2021-04-01 08:08:17 -0700

    [NodeJS] Update getSupplier response

--- api/src/dtos/index.ts
+++ api/src/dtos/index.ts
@@ -1,3 +1,5 @@
 export * from './CreateSupplierDto';
 export * from './CreateSupplierResponseDto';
 export * from './GetSuppliersDto';
+export * from './GetSupplierResponseDto';
+export * from './BaseResponseDto';
