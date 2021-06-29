1fc744572ffab63c591e1402d2ccfc7b7d14958c
Miguel Chan <miguel.chanc1991@gmail.com>
2021-06-28 20:49:50 -0500

    [UI-29] Implement ViewContact Dialog

--- packages/web-app/src/Utils/ObjectUtils.ts
+++ packages/web-app/src/Utils/ObjectUtils.ts
@@ -5,3 +5,17 @@
 export const isNullOrUndefined = (object: any): boolean => {
     return object === null || object === undefined;
 };
+
+/**
+ * Returns true if the provided string is null or null or undefined.
+ * 
+ * @param object .
+ * @returns .
+ */
+export const isNullOrEmpty = (object: string | null): boolean => {
+    if (isNullOrUndefined(object)) {
+        return true;
+    }
+
+    return object === '';
+};
